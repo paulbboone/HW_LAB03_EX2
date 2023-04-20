05981bfdb10c32ef4ee4d45d7a87c92202183f17
NguyenLam <91767799+paulbboone@users.noreply.github.com>
2023-04-20 20:24:34 +0700

    Initial commit

---
+++ app/src/main/java/edu/poly/lab3ex2/MainActivity.java
@@ -0,0 +1,56 @@
+package edu.poly.lab3ex2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.graphics.RectF;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CanvasView canvasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        canvasView = new CanvasView(this);
+        setContentView(canvasView);
+    }
+    public class CanvasView extends View {
+
+        private Paint paint;
+        private RectF rect;
+        private float x, y;
+
+        public CanvasView(Context context) {
+            super(context);
+            paint = new Paint();
+            paint.setColor(Color.RED);
+            rect = new RectF();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+//            canvas.drawColor(Color.WHITE);
+            canvas.drawRect(rect, paint);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            x = event.getX();
+            y = event.getY();
+            rect.set(x - 100, y - 50, x + 100, y + 50);
+            invalidate();
+            return true;
+        }
+    }
+}
